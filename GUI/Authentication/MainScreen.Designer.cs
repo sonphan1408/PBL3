49f4e61e73d87f88aacea921be10f0ccb301a210
BicHackNick <hadongocthai123@gmail.com>
2026-03-12 10:16:08 +0700

    Man hinh chinh ngan hang

--- GUI/Authentication/MainScreen.Designer.cs
+++ GUI/Authentication/MainScreen.Designer.cs
@@ -28,168 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
             this.panelHeader = new System.Windows.Forms.Panel();
-            this.labelAbout = new System.Windows.Forms.Label();
-            this.labelContacts = new System.Windows.Forms.Label();
-            this.labelServices = new System.Windows.Forms.Label();
-            this.labelHome = new System.Windows.Forms.Label();
-            this.labelBankName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panelContent = new System.Windows.Forms.Panel();
             this.labelDescription = new System.Windows.Forms.Label();
             this.labelTagline = new System.Windows.Forms.Label();
-            this.btnAdmin = new System.Windows.Forms.Button();
+            this.Admin = new System.Windows.Forms.Button();
             this.btnClient = new System.Windows.Forms.Button();
             this.panelHeader.SuspendLayout();
             this.panelContent.SuspendLayout();
             this.SuspendLayout();
-            
+            // 
             // panelHeader
-            this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
-            this.panelHeader.Controls.Add(this.labelAbout);
-            this.panelHeader.Controls.Add(this.labelContacts);
-            this.panelHeader.Controls.Add(this.labelServices);
-            this.panelHeader.Controls.Add(this.labelHome);
-            this.panelHeader.Controls.Add(this.labelBankName);
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.White;
+            this.panelHeader.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelHeader.BackgroundImage")));
+            this.panelHeader.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelHeader.Controls.Add(this.label1);
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeader.Location = new System.Drawing.Point(0, 0);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(1000, 80);
+            this.panelHeader.Size = new System.Drawing.Size(1182, 107);
             this.panelHeader.TabIndex = 0;
-            
-            // labelBankName
-            this.labelBankName.AutoSize = true;
-            this.labelBankName.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold);
-            this.labelBankName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.labelBankName.Location = new System.Drawing.Point(30, 20);
-            this.labelBankName.Name = "labelBankName";
-            this.labelBankName.Size = new System.Drawing.Size(200, 37);
-            this.labelBankName.TabIndex = 0;
-            this.labelBankName.Text = "❖ Sigma Bank";
-            
-            // labelHome
-            this.labelHome.AutoSize = true;
-            this.labelHome.Font = new System.Drawing.Font("Arial", 12F);
-            this.labelHome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.labelHome.Location = new System.Drawing.Point(360, 30);
-            this.labelHome.Name = "labelHome";
-            this.labelHome.Size = new System.Drawing.Size(50, 18);
-            this.labelHome.TabIndex = 1;
-            this.labelHome.Text = "Home";
-            
-            // labelServices
-            this.labelServices.AutoSize = true;
-            this.labelServices.Font = new System.Drawing.Font("Arial", 12F);
-            this.labelServices.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.labelServices.Location = new System.Drawing.Point(450, 30);
-            this.labelServices.Name = "labelServices";
-            this.labelServices.Size = new System.Drawing.Size(70, 18);
-            this.labelServices.TabIndex = 2;
-            this.labelServices.Text = "Services";
-            
-            // labelContacts
-            this.labelContacts.AutoSize = true;
-            this.labelContacts.Font = new System.Drawing.Font("Arial", 12F);
-            this.labelContacts.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.labelContacts.Location = new System.Drawing.Point(570, 30);
-            this.labelContacts.Name = "labelContacts";
-            this.labelContacts.Size = new System.Drawing.Size(70, 18);
-            this.labelContacts.TabIndex = 3;
-            this.labelContacts.Text = "Contacts";
-            
-            // labelAbout
-            this.labelAbout.AutoSize = true;
-            this.labelAbout.Font = new System.Drawing.Font("Arial", 12F);
-            this.labelAbout.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.labelAbout.Location = new System.Drawing.Point(684, 30);
-            this.labelAbout.Name = "labelAbout";
-            this.labelAbout.Size = new System.Drawing.Size(70, 18);
-            this.labelAbout.TabIndex = 4;
-            this.labelAbout.Text = "About Us";
-            
+            this.panelHeader.Paint += new System.Windows.Forms.PaintEventHandler(this.panelHeader_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(892, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 6;
+            // 
             // panelContent
-            this.panelContent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(220)))), ((int)(((byte)(220)))));
+            // 
+            this.panelContent.BackColor = System.Drawing.Color.White;
+            this.panelContent.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelContent.BackgroundImage")));
+            this.panelContent.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panelContent.Controls.Add(this.labelDescription);
             this.panelContent.Controls.Add(this.labelTagline);
-            this.panelContent.Controls.Add(this.btnAdmin);
+            this.panelContent.Controls.Add(this.Admin);
             this.panelContent.Controls.Add(this.btnClient);
-            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContent.Location = new System.Drawing.Point(0, 80);
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelContent.Location = new System.Drawing.Point(0, 107);
             this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(1000, 520);
+            this.panelContent.Size = new System.Drawing.Size(1182, 546);
             this.panelContent.TabIndex = 1;
-            
-            // labelTagline
-            this.labelTagline.AutoSize = true;
-            this.labelTagline.Font = new System.Drawing.Font("Arial", 42F, System.Drawing.FontStyle.Bold);
-            this.labelTagline.ForeColor = System.Drawing.Color.Black;
-            this.labelTagline.Location = new System.Drawing.Point(155, 50);
-            this.labelTagline.Name = "labelTagline";
-            this.labelTagline.Size = new System.Drawing.Size(700, 65);
-            this.labelTagline.TabIndex = 0;
-            this.labelTagline.Text = "Where Money Meet Trust.";
-            
+            this.panelContent.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContent_Paint);
+            // 
             // labelDescription
+            // 
             this.labelDescription.AutoSize = true;
+            this.labelDescription.BackColor = System.Drawing.Color.Transparent;
             this.labelDescription.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Italic);
             this.labelDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.labelDescription.Location = new System.Drawing.Point(155, 130);
             this.labelDescription.MaximumSize = new System.Drawing.Size(600, 60);
             this.labelDescription.Name = "labelDescription";
-            this.labelDescription.Size = new System.Drawing.Size(580, 36);
+            this.labelDescription.Size = new System.Drawing.Size(559, 48);
             this.labelDescription.TabIndex = 1;
-            this.labelDescription.Text = "Our bank is committed to delivering safety, convenience,\r\nand trust in every transaction.";
-            
+            this.labelDescription.Text = "Empowering your financial future with secure, seamless, and reliable banking solu" +
+    "tions.";
+            // 
+            // labelTagline
+            // 
+            this.labelTagline.AutoSize = true;
+            this.labelTagline.BackColor = System.Drawing.Color.Transparent;
+            this.labelTagline.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTagline.ForeColor = System.Drawing.Color.Black;
+            this.labelTagline.Location = new System.Drawing.Point(145, 49);
+            this.labelTagline.Name = "labelTagline";
+            this.labelTagline.Size = new System.Drawing.Size(915, 70);
+            this.labelTagline.TabIndex = 0;
+            this.labelTagline.Text = "Your Trusted Financial Partner.";
+            // 
+            // Admin
+            // 
+            this.Admin.BackColor = System.Drawing.Color.White;
+            this.Admin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.Admin.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.Admin.Location = new System.Drawing.Point(305, 220);
+            this.Admin.Name = "Admin";
+            this.Admin.Size = new System.Drawing.Size(100, 40);
+            this.Admin.TabIndex = 3;
+            this.Admin.Text = "Admin";
+            this.Admin.UseVisualStyleBackColor = false;
+            this.Admin.Click += new System.EventHandler(this.btnAdmin_Click);
+            // 
             // btnClient
-            this.btnClient.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            // 
+            this.btnClient.BackColor = System.Drawing.Color.RoyalBlue;
             this.btnClient.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.btnClient.ForeColor = System.Drawing.Color.White;
-            this.btnClient.Location = new System.Drawing.Point(175, 220);
+            this.btnClient.Location = new System.Drawing.Point(159, 220);
             this.btnClient.Name = "btnClient";
             this.btnClient.Size = new System.Drawing.Size(100, 40);
             this.btnClient.TabIndex = 2;
             this.btnClient.Text = "Client";
             this.btnClient.UseVisualStyleBackColor = false;
             this.btnClient.Click += new System.EventHandler(this.btnClient_Click);
-            
-            // btnAdmin
-            this.btnAdmin.BackColor = System.Drawing.Color.White;
-            this.btnAdmin.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnAdmin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.btnAdmin.Location = new System.Drawing.Point(310, 220);
-            this.btnAdmin.Name = "btnAdmin";
-            this.btnAdmin.Size = new System.Drawing.Size(100, 40);
-            this.btnAdmin.TabIndex = 3;
-            this.btnAdmin.Text = "Admin";
-            this.btnAdmin.UseVisualStyleBackColor = true;
-            this.btnAdmin.Click += new System.EventHandler(this.btnAdmin_Click);
-            
+            // 
             // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.ClientSize = new System.Drawing.Size(1182, 653);
             this.Controls.Add(this.panelContent);
             this.Controls.Add(this.panelHeader);
             this.Name = "MainScreen";
-            this.Text = "Sigma Bank - Where Money Meet Trust";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sigma Bank - Where Money Meet Trust";
+            this.Load += new System.EventHandler(this.MainScreen_Load);
             this.panelHeader.ResumeLayout(false);
             this.panelHeader.PerformLayout();
             this.panelContent.ResumeLayout(false);
             this.panelContent.PerformLayout();
             this.ResumeLayout(false);
+
         }
 
         #endregion
         
         private System.Windows.Forms.Panel panelHeader;
-        private System.Windows.Forms.Label labelBankName;
-        private System.Windows.Forms.Label labelHome;
-        private System.Windows.Forms.Label labelServices;
-        private System.Windows.Forms.Label labelContacts;
-        private System.Windows.Forms.Label labelAbout;
         private System.Windows.Forms.Panel panelContent;
         private System.Windows.Forms.Label labelTagline;
         private System.Windows.Forms.Label labelDescription;
         private System.Windows.Forms.Button btnClient;
-        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button Admin;
+        private System.Windows.Forms.Label label1;
     }
 }
--- GUI/Authentication/MainScreen.cs
+++ GUI/Authentication/MainScreen.cs
@@ -28,5 +28,30 @@ namespace GUI.Authentication
             // TODO: Xử lý khi bấm nút Admin
             MessageBox.Show("Admin login", "Sigma Bank");
         }
+
+        private void panelContent_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void labelBankName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelHeader_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
