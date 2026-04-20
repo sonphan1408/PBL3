e716edf7aacd2d2109a933a83f492f52ad4d1efc
THFiceTM <thficetm@gmail.com>
2026-04-20 21:41:09 +0700

    Fix some??

--- DTO/Models/InternalTransactionDTO.cs
+++ DTO/Models/InternalTransactionDTO.cs
@@ -8,6 +8,6 @@ namespace DTO.Models
 {
     public class InternalTransactionDTO : TransactionDTO
     {
-        public string ToAccount { get; set; }
+        public new string ToAccount { get; set; }
     }
 }
